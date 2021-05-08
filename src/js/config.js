7392b6b53e1e0f5c1ed1989e4f9aeb34f09b15b5
indieclimb <mfr660@hotmail.com>
2021-05-08 21:59:01 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5be15214-8a6e-4a24-a69b-3a66e204a609';
+export const MODAL_CLOSE_SEC = 2.5;
